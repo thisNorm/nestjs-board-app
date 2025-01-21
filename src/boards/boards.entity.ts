d57f7959b4b12782c47c01df8ebfe8118122039f
theNorm <kisook2557@gmail.com>
2025-01-21 12:38:38 +0900

    fix : 게시글 엔터티 필드 수정

--- src/boards/boards.entity.ts
+++ src/boards/boards.entity.ts
@@ -1,7 +1,8 @@
 import { BoardStatus } from "./boards-status.enum";
 
 export class Board {
-    id: string;
+    id: number;
+    author: string;
     title: string;
     content: string;
     status: BoardStatus
