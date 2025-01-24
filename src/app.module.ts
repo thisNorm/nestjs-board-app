ecce886c5f33041a710c10d12dce9e0f45cce4de
theNorm <kisook2557@gmail.com>
2025-01-24 10:45:02 +0900

    chore : DBConfig 작성 및 의존 관계 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { BlogsModule } from './blogs/blogs.module';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './configs/database.config';
+
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BoardsModule,
+    BlogsModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'DATABASE_CONFIG',
+      useValue: databaseConfig
+    }
+  ],
+  exports: ['DATABASE_CONFIG']
 })
-export class AppModule {}
+export class AppModule { }
