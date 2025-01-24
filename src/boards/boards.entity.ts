a6894488fb1d2d81c0171bf3c2824645dd0d1c39
theNorm <kisook2557@gmail.com>
2025-01-24 12:29:12 +0900

    chore : TypeORM의 엔터티 설정과, Service 계층의 Repository 주입

--- src/boards/boards.entity.ts
+++ src/boards/boards.entity.ts
@@ -1,9 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./boards-status.enum";
 
+@Entity()
 export class Board {
+    @PrimaryGeneratedColumn() // PK + Auto Increment
     id: number;
+
+    @Column() // General Column
     author: string;
+
+    @Column()
     title: string;
+
+    @Column()
     contents: string;
+
+    @Column()
     status: BoardStatus
 }
