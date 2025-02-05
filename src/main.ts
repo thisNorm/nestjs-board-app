c22a38719378e4fd4c24ffeb72aad824edf3e9f7
theNorm <kisook2557@gmail.com>
2025-02-05 12:01:44 +0900

    fix : Cookie Parser 메인 실행 버그 수정

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser);
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
